c32978026a70d78f018887620d49e1ff268d921e
Andrushka91 <rezmerita.andrey@gmail.com>
2021-01-17 19:28:22 +0100

    first test

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*string[] varsta = new int[10];
+             //string[] cars = { "Volvo", "BMW", "Ford", "Mazda","Dacia" };
+
+             /*cars[0] = "Mustang";
+             cars[1] = "Volvo";
+             cars[2] = "BMW";
+             cars[3] = "Ford";
+             cars[4] = "Mazda";
+
+             for (int i = 0; i < varsta.Length; i++)
+             {
+
+                 varsta[i] = Int32.Parse(Console.ReadLine());
+
+             }
+         foreach(int s in varsta)
+                 Console.WriteLine(s);*/
+
+            string name = "romania";
+             for ( int i=0;  i< name.Length; i++)
+                {
+                Console.WriteLine(name[i]);
+}
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Problema FP/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_FP
+{
+    class Program
+    {
+        //39. Scrieti o functie care să se verifice dacă un număr natural este sau nu cub perfect.
+
+        //functia determina daca numarul este cub perfect si afiseaza rezultatul
+        private static void cubPerfect(int n) 
+        {
+            // 0 ...... 27
+            //cube = 0*0*0
+            //cube = 1*1*1
+            //...
+            //cube = 3*3*3
+            int cub;
+
+            // parcurgem toate numerele intre 1 si n si determinam daca indicele i ridicat la puterea 3-a este egal cu numarul n
+            for (int i = 1; i <= n; i++)
+            {
+                cub = i * i * i;
+               //daca indicele i ridicat la puterea a 3-a este egal cu n se va afisa "este cub perfect" si se va iesi din functie
+                if (cub == n)
+                { Console.WriteLine(n + " este cub perfect");
+                    return;
+                }
+                //in caz contrar se va afisa mesajul respectiv si se va iesi din functie
+                else if (cub > n)
+                {
+                  Console.WriteLine(n + " nu este cub perfect");
+                    return;
+                }
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Introdu nr de la tastatura:");
+            string input = Console.ReadLine();
+            int n;
+            Int32.TryParse(input, out n);
+            cubPerfect(n);
+        }
+    }
+}
